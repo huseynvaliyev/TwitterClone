5fd441e1c0e504e7fd0bba4ddcd6582520e1dbb9
Huseyn <huseynvaliyev2015@gmail.com>
2021-10-02 17:40:26 +0400

    Project setup done

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  TwitterClone
 //
 //  Created by Huseyn Valiyev on 2.10.2021.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
     }
-
-
 }
-
